287b3befb5965eaa0b7e587a3d51ffb8ee2c004e
Restyled.io <commits@restyled.io>
2019-10-14 12:59:07 +0000

    Restyled by astyle

--- OtherCompetitions/2017 HDU Multi-University Contest 1/a.cpp
+++ OtherCompetitions/2017 HDU Multi-University Contest 1/a.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 
 int main() {
-	int a, cas = 0;
-	const double s = log10(2);
-	while(~scanf("%d", &a)) {
-		printf("Case #%d: %d\n", ++cas, (int)(a * s));
-	}
-	return 0;
+    int a, cas = 0;
+    const double s = log10(2);
+    while(~scanf("%d", &a)) {
+        printf("Case #%d: %d\n", ++cas, (int)(a * s));
+    }
+    return 0;
 }
--- OtherCompetitions/2017 HDU Multi-University Contest 1/f.cpp
+++ OtherCompetitions/2017 HDU Multi-University Contest 1/f.cpp
@@ -4,14 +4,14 @@ using namespace std;
 const int N = 2e5 + 10, mod = 1e9 + 7;
 
 long long qp(long long a, long long n) {
-	long long res = 1;
-	while(n) {
-		if(n & 1) 
-			res = res * a % mod;
-		a = a * a % mod;
-		n >>= 1;
-	}
-	return res;
+    long long res = 1;
+    while(n) {
+        if(n & 1)
+            res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
 }
 
 int n, m, a[N], b[N], vis[N], cas = 0;
@@ -19,46 +19,46 @@ int n, m, a[N], b[N], vis[N], cas = 0;
 int cnta[N], cntb[N], ans;
 
 int main() {
-	while(~scanf("%d%d", &n, &m)) {
-		ans = 1;
-		for(int i = 0; i < n; ++i) scanf("%d", &a[i]);
-		for(int i = 0; i < m; ++i) scanf("%d", &b[i]);
-		memset(cnta, 0, sizeof(int) * (n + 8));
-		memset(cntb, 0, sizeof(int) * (m + 8));	
-		memset(vis, 0, sizeof(int) * n);
-		for(int i = 0; i < n; ++i) {
-			if(vis[i]) continue;
-			vis[i] = 1;
-			int cnt = 1, p = a[i];
-			while(p != i) {
-				cnt++;
-				vis[p] = 1;
-				p = a[p];
-			}
-			cnta[cnt]++;
-		}
-		memset(vis, 0, sizeof(int) * m);
-		for(int i = 0; i < m; ++i) {
-			if(vis[i]) continue;
-			vis[i] = 1;
-			int cnt = 1, p = b[i];
-			while(p != i) {
-				cnt++;
-				vis[p] = 1;
-				p = b[p];
-			}
-			cntb[cnt]++;
-		}
-		for(int i = 1; i <= n; ++i) {
-			if(!cnta[i]) continue;
-			int tot = 0;
-			for(int j = 1; j <= i; ++j) {
-				if(i % j == 0) 
-					tot += j * cntb[j];
-			}
-			ans = 1LL * ans * qp(tot, cnta[i]) % mod;
-		}
-		printf("Case #%d: %d\n", ++cas, ans);
-	}
-	return 0;
+    while(~scanf("%d%d", &n, &m)) {
+        ans = 1;
+        for(int i = 0; i < n; ++i) scanf("%d", &a[i]);
+        for(int i = 0; i < m; ++i) scanf("%d", &b[i]);
+        memset(cnta, 0, sizeof(int) * (n + 8));
+        memset(cntb, 0, sizeof(int) * (m + 8));
+        memset(vis, 0, sizeof(int) * n);
+        for(int i = 0; i < n; ++i) {
+            if(vis[i]) continue;
+            vis[i] = 1;
+            int cnt = 1, p = a[i];
+            while(p != i) {
+                cnt++;
+                vis[p] = 1;
+                p = a[p];
+            }
+            cnta[cnt]++;
+        }
+        memset(vis, 0, sizeof(int) * m);
+        for(int i = 0; i < m; ++i) {
+            if(vis[i]) continue;
+            vis[i] = 1;
+            int cnt = 1, p = b[i];
+            while(p != i) {
+                cnt++;
+                vis[p] = 1;
+                p = b[p];
+            }
+            cntb[cnt]++;
+        }
+        for(int i = 1; i <= n; ++i) {
+            if(!cnta[i]) continue;
+            int tot = 0;
+            for(int j = 1; j <= i; ++j) {
+                if(i % j == 0)
+                    tot += j * cntb[j];
+            }
+            ans = 1LL * ans * qp(tot, cnta[i]) % mod;
+        }
+        printf("Case #%d: %d\n", ++cas, ans);
+    }
+    return 0;
 }
