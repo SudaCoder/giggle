ca37844877e1082845638fbdd58e0a0cf1c01324
Restyled.io <commits@restyled.io>
2019-11-02 17:03:31 +0000

    Restyled by astyle

--- Codeforces/CF-1209D.cpp
+++ Codeforces/CF-1209D.cpp
@@ -4,19 +4,19 @@ const int N = 1e5+9;
 using namespace std;
 int n, k, x, y;
 int fa[N];
- 
+
 void init() {
     for (int i = 1; i <= n; i++) fa[i] = i;
 }
- 
+
 int Find(int x) {
     return fa[x] == x ? x : fa[x] = Find(fa[x]);
 }
- 
+
 void Union(int x, int y) {
     fa[Find(x)] = Find(y);
 }
- 
+
 int main() {
     scanf("%d%d", &n, &k);
     init();
--- Codeforces/CF-396B.cpp
+++ Codeforces/CF-396B.cpp
@@ -3,30 +3,30 @@
 using namespace std;
 
 bool isPrime(int x) {
-  for (int i = 2; i * i <= x; i++) {
-    if (x % i == 0) {
-      return 1;
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0) {
+            return 1;
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
 long long ___gcd(long long a, long long b) {
-  return b ? ___gcd(b, a % b) : a;
+    return b ? ___gcd(b, a % b) : a;
 }
 
 int t, x;
 int main() {
-  scanf("%d", &t);
-  while(t--) {
-    scanf("%d", &x);
-    int p = x, q = x + 1;
-    for (; isPrime(p); p--);
-    for (; isPrime(q); q++);
-    long long up = 1LL * p * q - 2LL * (p + q) + 2 + 2LL * x;
-    long long down = 2LL * p * q;
-    long long gcd = ___gcd(up, down);
-    printf("%lld/%lld\n", up / gcd, down / gcd);
-  }
-  return 0;
+    scanf("%d", &t);
+    while(t--) {
+        scanf("%d", &x);
+        int p = x, q = x + 1;
+        for (; isPrime(p); p--);
+        for (; isPrime(q); q++);
+        long long up = 1LL * p * q - 2LL * (p + q) + 2 + 2LL * x;
+        long long down = 2LL * p * q;
+        long long gcd = ___gcd(up, down);
+        printf("%lld/%lld\n", up / gcd, down / gcd);
+    }
+    return 0;
 }
