8705d9decf0c0ddc842ac5ec9829769600c3d73e
Restyled.io <commits@restyled.io>
2019-11-02 17:03:32 +0000

    Restyled by clang-format

--- Codeforces/CF-1209D.cpp
+++ Codeforces/CF-1209D.cpp
@@ -1,35 +1,32 @@
 #include <bits/stdc++.h>
 using ll = long long;
-const int N = 1e5+9;
+const int N = 1e5 + 9;
 using namespace std;
 int n, k, x, y;
 int fa[N];
 
 void init() {
-    for (int i = 1; i <= n; i++) fa[i] = i;
+  for (int i = 1; i <= n; i++)
+    fa[i] = i;
 }
 
-int Find(int x) {
-    return fa[x] == x ? x : fa[x] = Find(fa[x]);
-}
+int Find(int x) { return fa[x] == x ? x : fa[x] = Find(fa[x]); }
 
-void Union(int x, int y) {
-    fa[Find(x)] = Find(y);
-}
+void Union(int x, int y) { fa[Find(x)] = Find(y); }
 
 int main() {
-    scanf("%d%d", &n, &k);
-    init();
-    for (int i = 0; i < k; i++) {
-        scanf("%d%d", &x, &y);
-        Union(x, y);
+  scanf("%d%d", &n, &k);
+  init();
+  for (int i = 0; i < k; i++) {
+    scanf("%d%d", &x, &y);
+    Union(x, y);
+  }
+  int ans = k - n;
+  for (int i = 1; i <= n; i++) {
+    if (Find(i) == i) {
+      ans++;
     }
-    int ans = k - n;
-    for (int i = 1; i <= n; i++) {
-        if(Find(i) == i) {
-            ans ++;
-        }
-    }
-    printf("%d\n", ans);
-    return 0;
+  }
+  printf("%d\n", ans);
+  return 0;
 }
