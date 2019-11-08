3910d3be3acb6d0793c42589e7672d65fed8ec2b
Restyled.io <commits@restyled.io>
2019-11-08 08:22:59 +0000

    Restyled by clang-format

--- Codeforces/CF-698A.cpp
+++ Codeforces/CF-698A.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <cstring>
 #include <cstdio>
+#include <cstring>
+#include <iostream>
 const int maxn = 1e4;
 int a[maxn];
 int dp[maxn][3];
 const int inf = 1e9;
 using namespace std;
 int main() {
-    int n;
-    scanf("%d",&n);
-    for(int i = 1; i <= n; i++) {
-        scanf("%d",&a[i]);
+  int n;
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &a[i]);
+  }
+  for (int i = 1; i <= n; i++) {
+    dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][1], dp[i - 1][2]));
+    if (a[i] == 0) {
+      dp[i][1] = -inf;
+      dp[i][2] = -inf;
+    } else if (a[i] == 1) {
+      dp[i][1] = max(dp[i - 1][0], dp[i - 1][2]) + 1;
+      dp[i][2] = -inf;
+    } else if (a[i] == 2) {
+      dp[i][1] = -inf;
+      dp[i][2] = max(dp[i - 1][0], dp[i - 1][1]) + 1;
+    } else {
+      dp[i][1] = max(dp[i - 1][0], dp[i - 1][2]) + 1;
+      dp[i][2] = max(dp[i - 1][0], dp[i - 1][1]) + 1;
     }
-    for(int i = 1; i <= n; i++) {
-        dp[i][0] = max(dp[i-1][0], max(dp[i-1][1], dp[i-1][2]));
-        if(a[i]==0) {
-            dp[i][1] = -inf;
-            dp[i][2] = -inf;
-        } else if(a[i]==1) {
-            dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + 1;
-            dp[i][2] = -inf;
-        } else if(a[i]==2) {
-            dp[i][1] = -inf;
-            dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + 1;
-        } else {
-            dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + 1;
-            dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + 1;
-        }
-    }
-    printf("%d\n",n-max(max(0, dp[n][0]), max(dp[n][1], dp[n][2])));
+  }
+  printf("%d\n", n - max(max(0, dp[n][0]), max(dp[n][1], dp[n][2])));
 }
