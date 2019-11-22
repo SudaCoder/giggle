77ac0bfda2694d988220773947187882c2fa3252
Restyled.io <commits@restyled.io>
2019-11-22 20:01:06 +0000

    Restyled by astyle

--- Codeforces/CF-1195C.cpp
+++ Codeforces/CF-1195C.cpp
@@ -7,18 +7,18 @@ using namespace std;
 int n, a[N], b[N];
 long long dp[N][2];
 int main() {
-  scanf("%d", &n);
-  memset(dp, 0, sizeof(dp));
-  for (int i = 1; i <= n; i++) {
-    scanf("%d", &a[i]);
-  }
-  for (int i = 1; i <= n; i++) {
-    scanf("%d", &b[i]);
-  }
-  for (int i = 1; i <= n; i++) {
-    dp[i][0] = max(dp[i-1][0], dp[i-1][1] + a[i]);
-    dp[i][1] = max(dp[i-1][1], dp[i-1][0] + b[i]);
-  }
-  printf("%lld\n", max(dp[n][0], dp[n][1]));
-  return 0;
+    scanf("%d", &n);
+    memset(dp, 0, sizeof(dp));
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &b[i]);
+    }
+    for (int i = 1; i <= n; i++) {
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1] + a[i]);
+        dp[i][1] = max(dp[i-1][1], dp[i-1][0] + b[i]);
+    }
+    printf("%lld\n", max(dp[n][0], dp[n][1]));
+    return 0;
 }
