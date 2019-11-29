303d845599b11aed5a333a0d81f5916e977ea12c
Restyled.io <commits@restyled.io>
2019-11-29 18:41:52 +0000

    Restyled by astyle

--- Codeforces/CF-1242A.cpp
+++ Codeforces/CF-1242A.cpp
@@ -8,28 +8,28 @@ using namespace std;
 ll n;
 
 ll gcd(ll a, ll b) {
-  return b ? gcd(b, a % b) : a;
+    return b ? gcd(b, a % b) : a;
 }
 
 int main() {
-  scanf("%lld", &n);
-  if (n == 1) {
-    return puts("1"), 0;
-  }
-  vector<ll> d;
-  for (ll i = 2; i * i <= n; i++) {
-    if (n % i == 0) {
-      d.push_back(i);
-      while(n % i == 0) {
-        n /= i;
-      }
+    scanf("%lld", &n);
+    if (n == 1) {
+        return puts("1"), 0;
     }
-  }
-  if (n > 1) d.push_back(n);
-  ll ans = d[0];
-  for (int i = 1; i < d.size(); i++) {
-    ans = gcd(ans, d[i]);
-  }
-  printf("%lld\n", ans);
-  return 0;
+    vector<ll> d;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            d.push_back(i);
+            while(n % i == 0) {
+                n /= i;
+            }
+        }
+    }
+    if (n > 1) d.push_back(n);
+    ll ans = d[0];
+    for (int i = 1; i < d.size(); i++) {
+        ans = gcd(ans, d[i]);
+    }
+    printf("%lld\n", ans);
+    return 0;
 }
