5e407d811a4bf088a593c2e3efc38d8424958724
Restyled.io <commits@restyled.io>
2019-12-05 07:22:50 +0000

    Restyled by astyle

--- Codeforces/CF-1245C.cpp
+++ Codeforces/CF-1245C.cpp
@@ -8,17 +8,17 @@ char s[N];
 int dp[N], n;
 
 int main() {
-  dp[0] = 1;
-  scanf("%s", s+1);
-  int len = strlen(s + 1);
-  for (int i = 1; i <= len; i++) {
-    if (s[i] == 'n' || s[i] == 'u') {
-      if (s[i] == s[i-1]) {
-        dp[i] = (dp[i-1] + dp[i-2]) % mod;
-      } else dp[i] = dp[i-1];
-    } else if(s[i] == 'm' || s[i] == 'w') {
-      dp[i] = 0;
-    } else dp[i] = dp[i-1];
-  }
-  return printf("%d\n", dp[len]), 0;
+    dp[0] = 1;
+    scanf("%s", s+1);
+    int len = strlen(s + 1);
+    for (int i = 1; i <= len; i++) {
+        if (s[i] == 'n' || s[i] == 'u') {
+            if (s[i] == s[i-1]) {
+                dp[i] = (dp[i-1] + dp[i-2]) % mod;
+            } else dp[i] = dp[i-1];
+        } else if(s[i] == 'm' || s[i] == 'w') {
+            dp[i] = 0;
+        } else dp[i] = dp[i-1];
+    }
+    return printf("%d\n", dp[len]), 0;
 }
