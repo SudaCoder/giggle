ca35ad1c56aba4ca9a94ac6e35f467b72a60caa4
Restyled.io <commits@restyled.io>
2019-12-05 18:43:15 +0000

    Restyled by clang-format

--- Codeforces/CF-1225D.cpp
+++ Codeforces/CF-1225D.cpp
@@ -9,44 +9,44 @@ int n, k, x;
 unordered_map<int, int> mp;
 
 pair<int, int> trans(int x) {
-    int a = 1, b = 1, cnt;
-    for (int i = 2; i * i <= x; i++) {
-        cnt = 0;
-        while (x % i == 0) {
-            cnt++;
-            x /= i;
-        }
-        cnt %= k;
-        if (cnt) {
-            for (int j = 0; j < cnt; j++) {
-                a *= i;
-            }
-            for (int j = cnt; j < k; j++) {
-                b *= i;
-            }
-        }
+  int a = 1, b = 1, cnt;
+  for (int i = 2; i * i <= x; i++) {
+    cnt = 0;
+    while (x % i == 0) {
+      cnt++;
+      x /= i;
     }
-    if (x > 1) {
-        a *= x;
-        for (int j = 1; j < k && b <= MX; j++) {
-            b *= x;
-        }
+    cnt %= k;
+    if (cnt) {
+      for (int j = 0; j < cnt; j++) {
+        a *= i;
+      }
+      for (int j = cnt; j < k; j++) {
+        b *= i;
+      }
     }
-    if (b >= MX)
-        b = -1;
-    return make_pair(a, b);
+  }
+  if (x > 1) {
+    a *= x;
+    for (int j = 1; j < k && b <= MX; j++) {
+      b *= x;
+    }
+  }
+  if (b >= MX)
+    b = -1;
+  return make_pair(a, b);
 }
 
 int main() {
-    scanf("%d %d", &n, &k);
-    ll ans = 0;
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &x);
-        pair<int, int> ab = trans(x);
-        ans += mp[ab.first];
-        if (ab.second > 0)
-            mp[ab.second]++;
-    }
-    printf("%lld\n", ans);
-    return 0;
+  scanf("%d %d", &n, &k);
+  ll ans = 0;
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &x);
+    pair<int, int> ab = trans(x);
+    ans += mp[ab.first];
+    if (ab.second > 0)
+      mp[ab.second]++;
+  }
+  printf("%lld\n", ans);
+  return 0;
 }
