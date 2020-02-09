de9d3e407823ec8a3d81110b868efb62498dddbc
hu-chi <w.r.j@qq.com>
2020-02-09 20:01:38 +0800

    new probelms

---
+++ Codeforces/CF-1104B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+const int N = 1e5+9;
+char c[N];
+
+int main(){
+  int ans = 0, i = 0;
+  while(cin >> c[++i]) {
+    if (c[i] == c[i-1]) {
+      i -= 2;
+      ans += 1;
+    }
+  }
+  if (ans % 2 == 0) cout << "No" << endl;
+  else cout << "Yes" << endl;
+}
