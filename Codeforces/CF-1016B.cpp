81c09e2b77f8fa0d22c157e49de3191a2e2e26e9
Restyled.io <commits@restyled.io>
2020-02-09 12:02:47 +0000

    Restyled by astyle

--- Codeforces/CF-1016B.cpp
+++ Codeforces/CF-1016B.cpp
@@ -6,20 +6,20 @@ int n, m, q, sum[N];
 string s, t;
 
 int main() {
-  cin >> n >> m >> q;
-  cin >> s >> t;
-  int pos = 0;
-  while((pos = s.find(t, pos)) != string::npos) {
-    sum[++pos] = 1;
-  }
-  for (int i = 1; i <= s.length(); i++) {
-    sum[i] += sum[i-1];
-  }
-  int l, r;
-  while(q--) {
-    cin >> l >> r;
-    r -= t.length() - 1;
-    cout << (r >= l ? sum[r]-sum[l-1] : 0) << endl;
-  }
-  return 0;
+    cin >> n >> m >> q;
+    cin >> s >> t;
+    int pos = 0;
+    while((pos = s.find(t, pos)) != string::npos) {
+        sum[++pos] = 1;
+    }
+    for (int i = 1; i <= s.length(); i++) {
+        sum[i] += sum[i-1];
+    }
+    int l, r;
+    while(q--) {
+        cin >> l >> r;
+        r -= t.length() - 1;
+        cout << (r >= l ? sum[r]-sum[l-1] : 0) << endl;
+    }
+    return 0;
 }
--- Codeforces/CF-1104B.cpp
+++ Codeforces/CF-1104B.cpp
@@ -3,14 +3,14 @@ using namespace std;
 const int N = 1e5+9;
 char c[N];
 
-int main(){
-  int ans = 0, i = 0;
-  while(cin >> c[++i]) {
-    if (c[i] == c[i-1]) {
-      i -= 2;
-      ans += 1;
+int main() {
+    int ans = 0, i = 0;
+    while(cin >> c[++i]) {
+        if (c[i] == c[i-1]) {
+            i -= 2;
+            ans += 1;
+        }
     }
-  }
-  if (ans % 2 == 0) cout << "No" << endl;
-  else cout << "Yes" << endl;
+    if (ans % 2 == 0) cout << "No" << endl;
+    else cout << "Yes" << endl;
 }
