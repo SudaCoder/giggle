ca912fcbf7cf3fce3463d91b9cc2b453ad573585
Restyled.io <commits@restyled.io>
2020-02-09 12:02:47 +0000

    Restyled by clang-format

--- Codeforces/CF-1159B.cpp
+++ Codeforces/CF-1159B.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int n, x;
 
 int main() {
-    scanf("%d", &n);
-    int ans = 1e9;
-    for (int i = 1; i <= n; i++) {
-        scanf("%d", &x);
-        ans = min(ans, x/max(i-1, n-i));
-    }
-    printf("%d\n", ans);
-    return 0;
+  scanf("%d", &n);
+  int ans = 1e9;
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &x);
+    ans = min(ans, x / max(i - 1, n - i));
+  }
+  printf("%d\n", ans);
+  return 0;
 }
