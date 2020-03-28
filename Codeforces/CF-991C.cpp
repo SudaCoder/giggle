465e930dc84759cbadd098ea0907765d98b327e6
tusikalanse <615822545@qq.com>
2020-03-28 23:06:32 +0800

    4 easy problems

---
+++ Codeforces/CF-991C.cpp
@@ -0,0 +1,31 @@
+//991C
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n;
+
+bool check(long long k) {
+    long long now = n, tot = 0;
+    while (now) {
+        tot += min(k, now);
+        now -= min(k, now);
+        now -= now / 10;
+    }
+    return tot * 2 >= n;
+}
+
+int main() {
+    cin >> n;
+    long long l = 1, r = n, ans;
+    while (l <= r) {
+        long long mid = (l + r) >> 1;
+        if (check(mid)) {
+            r = mid - 1;
+            ans = mid;
+        }
+        else
+            l = mid + 1;
+    }
+    cout << ans << endl;
+    return 0;
+}
