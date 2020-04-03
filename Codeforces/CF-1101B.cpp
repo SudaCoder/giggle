6588ce8228425b0fa2a967e938ba3cc2b4516476
Restyled.io <commits@restyled.io>
2020-04-03 10:48:27 +0000

    Restyled by clang-format

--- Codeforces/CF-1101B.cpp
+++ Codeforces/CF-1101B.cpp
@@ -1,4 +1,4 @@
-//1101B
+// 1101B
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,43 +8,43 @@ int n, p[4], ans;
 char s[N];
 
 int main() {
-    scanf("%s", s + 1);
-    n = strlen(s + 1);
-    for (int i = 1; i <= n; ++i) {
-        if (s[i] == '[') {
-            p[0] = i;
-            break;
-        }
+  scanf("%s", s + 1);
+  n = strlen(s + 1);
+  for (int i = 1; i <= n; ++i) {
+    if (s[i] == '[') {
+      p[0] = i;
+      break;
     }
-    for (int i = n; i; --i) {
-        if (s[i] == ']') {
-            p[3] = i;
-            break;
-        }
+  }
+  for (int i = n; i; --i) {
+    if (s[i] == ']') {
+      p[3] = i;
+      break;
     }
-    if (p[0] == 0 || p[3] == 0) {
-        puts("-1");
-        return 0;
-    }
-    for (int i = p[0]; i <= p[3]; ++i) {
-        if (s[i] == ':') {
-            p[1] = i;
-            break;
-        }
-    }
-    for (int i = p[3]; i >= p[0]; --i) {
-        if (s[i] == ':') {
-            p[2] = i;
-            break;
-        }
-    }
-    if (p[1] == 0 || p[1] == p[2]) {
-        puts("-1");
-        return 0;
-    }
-    ans = 4;
-    for (int i = p[1]; i <= p[2]; ++i)
-        ans += s[i] == '|';
-    printf("%d\n", ans);
+  }
+  if (p[0] == 0 || p[3] == 0) {
+    puts("-1");
     return 0;
+  }
+  for (int i = p[0]; i <= p[3]; ++i) {
+    if (s[i] == ':') {
+      p[1] = i;
+      break;
+    }
+  }
+  for (int i = p[3]; i >= p[0]; --i) {
+    if (s[i] == ':') {
+      p[2] = i;
+      break;
+    }
+  }
+  if (p[1] == 0 || p[1] == p[2]) {
+    puts("-1");
+    return 0;
+  }
+  ans = 4;
+  for (int i = p[1]; i <= p[2]; ++i)
+    ans += s[i] == '|';
+  printf("%d\n", ans);
+  return 0;
 }
--- Codeforces/CF-991C.cpp
+++ Codeforces/CF-991C.cpp
@@ -1,31 +1,30 @@
-//991C
+// 991C
 #include <bits/stdc++.h>
 using namespace std;
 
 long long n;
 
 bool check(long long k) {
-    long long now = n, tot = 0;
-    while (now) {
-        tot += min(k, now);
-        now -= min(k, now);
-        now -= now / 10;
-    }
-    return tot * 2 >= n;
+  long long now = n, tot = 0;
+  while (now) {
+    tot += min(k, now);
+    now -= min(k, now);
+    now -= now / 10;
+  }
+  return tot * 2 >= n;
 }
 
 int main() {
-    cin >> n;
-    long long l = 1, r = n, ans;
-    while (l <= r) {
-        long long mid = (l + r) >> 1;
-        if (check(mid)) {
-            r = mid - 1;
-            ans = mid;
-        }
-        else
-            l = mid + 1;
-    }
-    cout << ans << endl;
-    return 0;
+  cin >> n;
+  long long l = 1, r = n, ans;
+  while (l <= r) {
+    long long mid = (l + r) >> 1;
+    if (check(mid)) {
+      r = mid - 1;
+      ans = mid;
+    } else
+      l = mid + 1;
+  }
+  cout << ans << endl;
+  return 0;
 }
