53eeb0849966714037a7fcc705959eea4f460446
Restyled.io <commits@restyled.io>
2020-04-11 15:02:31 +0000

    Restyled by astyle

--- Codeforces/CF-834C.cpp
+++ Codeforces/CF-834C.cpp
@@ -4,21 +4,21 @@
 const int N = 1e3+9;
 using namespace std;
 int x3[N];
- 
-int gcd(int a, int b){
+
+int gcd(int a, int b) {
     return b ? gcd(b, a % b) : a;
 }
 
 void init() {
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < N; i++) {
         x3[i] = i * i * i;
     }
 }
- 
+
 bool judge(int a) {
     return *lower_bound(x3, x3 + N, a) != a;
 }
- 
+
 int main() {
     init();
     int t, a, b;
@@ -30,7 +30,7 @@ int main() {
         int x = a / g;
         int y = b / g;
         if(g % x != 0 || g % y != 0) flag = 1;
-        else{
+        else {
             g /= x;
             g /= y;
             flag = judge(g);
--- Template/algorithms/graph/flyod.cpp
+++ Template/algorithms/graph/flyod.cpp
@@ -5,7 +5,7 @@
 
 template<typename T>
 void flyod(std::vector<std::vector<T> > &graph) {
-    int n = graph.size(); 
+    int n = graph.size();
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
