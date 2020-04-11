d8590a9caf28e000b5ff56714b8d438176e10394
Restyled.io <commits@restyled.io>
2020-04-11 15:02:32 +0000

    Restyled by clang-format

--- Codeforces/CF-834C.cpp
+++ Codeforces/CF-834C.cpp
@@ -1,42 +1,41 @@
-#include<cstdio>
-#include<iostream>
-#include<algorithm>
-const int N = 1e3+9;
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+const int N = 1e3 + 9;
 using namespace std;
 int x3[N];
 
-int gcd(int a, int b) {
-    return b ? gcd(b, a % b) : a;
-}
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
 
 void init() {
-    for(int i = 0; i < N; i++) {
-        x3[i] = i * i * i;
-    }
+  for (int i = 0; i < N; i++) {
+    x3[i] = i * i * i;
+  }
 }
 
-bool judge(int a) {
-    return *lower_bound(x3, x3 + N, a) != a;
-}
+bool judge(int a) { return *lower_bound(x3, x3 + N, a) != a; }
 
 int main() {
-    init();
-    int t, a, b;
-    scanf("%d", &t);
-    while(t--) {
-        bool flag = 0;
-        scanf("%d%d", &a, &b);
-        int g = gcd(a,b);
-        int x = a / g;
-        int y = b / g;
-        if(g % x != 0 || g % y != 0) flag = 1;
-        else {
-            g /= x;
-            g /= y;
-            flag = judge(g);
-        }
-        if(flag) puts("No");
-        else puts("Yes");
+  init();
+  int t, a, b;
+  scanf("%d", &t);
+  while (t--) {
+    bool flag = 0;
+    scanf("%d%d", &a, &b);
+    int g = gcd(a, b);
+    int x = a / g;
+    int y = b / g;
+    if (g % x != 0 || g % y != 0)
+      flag = 1;
+    else {
+      g /= x;
+      g /= y;
+      flag = judge(g);
     }
-    return 0;
+    if (flag)
+      puts("No");
+    else
+      puts("Yes");
+  }
+  return 0;
 }
--- Codeforces/CF-934C.cpp
+++ Codeforces/CF-934C.cpp
@@ -4,20 +4,20 @@
 
 #include <iostream>
 #include <vector>
-const int N = 2e3+9;
+const int N = 2e3 + 9;
 
 using namespace std;
-int n, dp[5][N], a[N], b[5]= {0,1,2,1,2};
+int n, dp[5][N], a[N], b[5] = {0, 1, 2, 1, 2};
 
-int main () {
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
-        scanf("%d", &a[i]);
+int main() {
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &a[i]);
+  }
+  for (int i = 1; i <= 4; i++) {
+    for (int j = 1; j <= n; j++) {
+      dp[i][j] = max(dp[i - 1][j], dp[i][j - 1] + (a[j] == b[i]));
     }
-    for (int i = 1; i <= 4; i++) {
-        for (int j = 1; j <= n; j++) {
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1]+(a[j]==b[i]));
-        }
-    }
-    printf("%d\n", dp[4][n]);
-    return 0;
+  }
+  printf("%d\n", dp[4][n]);
+  return 0;
