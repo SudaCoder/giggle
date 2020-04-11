650e2b2846a92a507ed7eebfbac09a60a412d1f6
Restyled.io <commits@restyled.io>
2020-04-11 13:39:51 +0000

    Restyled by clang-format

--- Codeforces/CF-1040B.cpp
+++ Codeforces/CF-1040B.cpp
@@ -1,4 +1,4 @@
-//1040B
+// 1040B
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,22 +7,22 @@ int n, k;
 vector<int> ans, vec;
 
 int main() {
-    cin >> n >> k;
-    for (int i = 1; i <= k + 1; ++i) {
-        vec.clear();
-        vec.push_back(i);
-        int r = i + k + 1;
-        for (int j = r + k; j <= n; j = r + k) {
-            r = j + k + 1;
-            vec.push_back(j);
-        }
-        if (r > n)
-            if (ans.empty() || vec.size() < ans.size())
-                ans = vec;
+  cin >> n >> k;
+  for (int i = 1; i <= k + 1; ++i) {
+    vec.clear();
+    vec.push_back(i);
+    int r = i + k + 1;
+    for (int j = r + k; j <= n; j = r + k) {
+      r = j + k + 1;
+      vec.push_back(j);
     }
-    cout << ans.size() << endl;
-    for (int i = 0; i < ans.size(); ++i)
-        cout << ans[i] << " ";
-    cout << endl;
-    return 0;
+    if (r > n)
+      if (ans.empty() || vec.size() < ans.size())
+        ans = vec;
+  }
+  cout << ans.size() << endl;
+  for (int i = 0; i < ans.size(); ++i)
+    cout << ans[i] << " ";
+  cout << endl;
+  return 0;
 }
