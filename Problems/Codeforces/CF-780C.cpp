68ec226eb54195444ae5db85605217b8d3070eb6
Restyled.io <commits@restyled.io>
2020-04-30 15:11:26 +0000

    Restyled by clang-format

--- Problems/Codeforces/CF-780C.cpp
+++ Problems/Codeforces/CF-780C.cpp
@@ -1,4 +1,4 @@
-//780C
+// 780C
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,31 +8,33 @@ int c[N], D, n;
 vector<int> edge[N];
 
 void dfs(int rt, int fa) {
-    int x = c[rt], y = c[fa], k = 1;
-    for (int i = 0; i < edge[rt].size(); ++i) {
-        int j = edge[rt][i];
-        if (j == fa) continue;
-        while (k == x || k == y) k++;
-        c[j] = k;
-        k++;
-        dfs(j, rt);
-    }
+  int x = c[rt], y = c[fa], k = 1;
+  for (int i = 0; i < edge[rt].size(); ++i) {
+    int j = edge[rt][i];
+    if (j == fa)
+      continue;
+    while (k == x || k == y)
+      k++;
+    c[j] = k;
+    k++;
+    dfs(j, rt);
+  }
 }
 
 int main() {
-    scanf("%d", &n);
-    for (int i = 1; i < n; ++i) {
-        int x, y;
-        scanf("%d%d", &x, &y);
-        edge[x].push_back(y);
-        edge[y].push_back(x);
-    }
-    for (int i = 1; i <= n; ++i)
-        D = max(D, (int)edge[i].size());
-    c[1] = 1;
-    dfs(1, 0);
-    printf("%d\n", D + 1);
-    for (int i = 1; i <= n; ++i)
-        printf("%d%c", c[i], " \n"[i == n]);
-    return 0;
+  scanf("%d", &n);
+  for (int i = 1; i < n; ++i) {
+    int x, y;
+    scanf("%d%d", &x, &y);
+    edge[x].push_back(y);
+    edge[y].push_back(x);
+  }
+  for (int i = 1; i <= n; ++i)
+    D = max(D, (int)edge[i].size());
+  c[1] = 1;
+  dfs(1, 0);
+  printf("%d\n", D + 1);
+  for (int i = 1; i <= n; ++i)
+    printf("%d%c", c[i], " \n"[i == n]);
+  return 0;
 }
