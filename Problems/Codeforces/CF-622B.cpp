cc7b8d3f33ee4618510efce77079a2c7ea1fdbc1
Restyled.io <commits@restyled.io>
2020-05-15 09:46:52 +0000

    Restyled by clang-format

--- Problems/Codeforces/CF-622B.cpp
+++ Problems/Codeforces/CF-622B.cpp
@@ -1,15 +1,15 @@
-//622B
+// 622B
 #include <bits/stdc++.h>
 using namespace std;
 
 int h, m, a;
 
 int main() {
-    scanf("%d:%d%d", &h, &m, &a);
-    m += a;
-    h += m / 60;
-    m %= 60;
-    h %= 24;
-    printf("%02d:%02d\n", h, m);
-    return 0;
+  scanf("%d:%d%d", &h, &m, &a);
+  m += a;
+  h += m / 60;
+  m %= 60;
+  h %= 24;
+  printf("%02d:%02d\n", h, m);
+  return 0;
 }
--- Problems/Codeforces/CF-716B.cpp
+++ Problems/Codeforces/CF-716B.cpp
@@ -1,4 +1,4 @@
-//716B
+// 716B
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,65 +7,66 @@ int cnt[30], num, n, flag;
 char s[N];
 
 void gao() {
-    for (int i = 0; i < N; ++i)
-        if (s[i] == '?')
-            s[i] = 'A';
+  for (int i = 0; i < N; ++i)
+    if (s[i] == '?')
+      s[i] = 'A';
 }
 
 void deal(int l, int r) {
-    for (int i = l; i <= r; ++i) {
-        if (s[i] == '?') {
-            for (int c = 0; c < 26; ++c) {
-                if (cnt[c] == 0) {
-                    cnt[c] = 1;
-                    s[i] = 'A' + c;
-                    break;
-                }
-            }
+  for (int i = l; i <= r; ++i) {
+    if (s[i] == '?') {
+      for (int c = 0; c < 26; ++c) {
+        if (cnt[c] == 0) {
+          cnt[c] = 1;
+          s[i] = 'A' + c;
+          break;
         }
+      }
     }
+  }
 }
 
 int main() {
-    scanf("%s", s);
-    n = strlen(s);
-    if (n < 26) {
-        puts("-1");
-        return 0;
+  scanf("%s", s);
+  n = strlen(s);
+  if (n < 26) {
+    puts("-1");
+    return 0;
+  }
+  for (int i = 0; i < 26; ++i) {
+    if (s[i] == '?')
+      continue;
+    cnt[s[i] - 'A']++;
+    if (cnt[s[i] - 'A'] == 2)
+      num++;
+  }
+  if (num == 0) {
+    flag = 1;
+    deal(0, 25);
+  }
+  for (int i = 26; i < n; ++i) {
+    if (flag)
+      break;
+    if (s[i - 26] != '?') {
+      cnt[s[i - 26] - 'A']--;
+      if (cnt[s[i - 26] - 'A'] == 1)
+        num--;
     }
-    for (int i = 0; i < 26; ++i) {
-        if (s[i] == '?') continue;
-        cnt[s[i] - 'A']++;
-        if (cnt[s[i] - 'A'] == 2)
-            num++;
+    if (s[i] != '?') {
+      cnt[s[i] - 'A']++;
+      if (cnt[s[i] - 'A'] == 2)
+        num++;
     }
     if (num == 0) {
-        flag = 1;
-        deal(0, 25);
+      flag = 1;
+      deal(i - 25, i);
     }
-    for (int i = 26; i < n; ++i) {
-        if (flag) break;
-        if (s[i - 26] != '?') {
-            cnt[s[i - 26] - 'A']--;
-            if (cnt[s[i - 26] - 'A'] == 1)
-                num--;
-        }
-        if (s[i] != '?') {
-            cnt[s[i] - 'A']++;
-            if (cnt[s[i] - 'A'] == 2)
-                num++;
-        }
-        if (num == 0) {
-            flag = 1;
-            deal(i - 25, i);
-        }
-    }
-    if (flag) {
-        gao();
-        printf("%s\n", s);
-    }
-    else {
-        puts("-1");
-    }
-    return 0;
+  }
+  if (flag) {
+    gao();
+    printf("%s\n", s);
+  } else {
+    puts("-1");
+  }
+  return 0;
 }
