92034c0af2786026ad703b59bdb22ee9bfe8f76b
tusikalanse <32886598+tusikalanse@users.noreply.github.com>
2020-09-10 10:59:27 +0800

    Update floyd.cpp
    fix typo

--- Template/algorithms/graph/floyd.cpp
+++ Template/algorithms/graph/floyd.cpp
@@ -3,7 +3,7 @@
 #ifndef flyod_alg
 #define flyod_alg
 
-template <typename T> void flyod(std::vector<std::vector<T>> &graph) {
+template <typename T> void floyd(std::vector<std::vector<T>> &graph) {
   int n = graph.size();
   for (int k = 0; k < n; k++) {
     for (int i = 0; i < n; i++) {
